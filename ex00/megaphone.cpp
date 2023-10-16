449da54c2cc07e0e7682a71f705a3fa79c796b86
shmorish <110565242+Mori062@users.noreply.github.com>
2023-10-16 01:53:05 +0000

    [Done] ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std ::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+        int i = 1;
+        while (i < argc)
+        {
+            std::string str(argv[i]);
+            int j = 0;
+            while (j < (int)str.length())
+            {
+                std::cout << (char)toupper(str[j]);
+                j++;
+            }
+            i++;
+        }
+		std::cout << std::endl;
+	}
+	return (0);
+}
